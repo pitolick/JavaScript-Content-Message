223f88af8dd67c4dfff584b7cb0bcdf3d264b9e9
ぴいた <pitolick@gmail.com>
2020-09-12 17:20:19 +0900

    add release.yml

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: JavaScript Content Message
+Plugin URI: https://github.com/pitolick/JavaScript-Content-Message
+Description: 特定の期間が経過した投稿・固定ページにメッセージを出力します
+Version: {release version}
+Author: ぴいた
+License: GPL2
+*/
+
